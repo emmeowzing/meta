e1e0e1cd080cee2e8681e8383a4503560a60aaa4
bjd2385 <bjd2385@aperiodicity.com>
2017-01-24 12:15:32 -0500

    Modified example

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 
 FILE="img.c"
 BASE={FILE%.*}
+IMAGE="example.png"
 
 if [ -e $BASE ]
 then
@@ -10,22 +11,35 @@ then
 fi
 
 # Compile my program
-gcc img.c -o img -O3 -g -Wall -lpng12 -lm
+gcc $FILE -o $BASE -O3 -g -Wall -lpng12 -lm
 
 if [ -e img ]
 # Successful compile
 then
     DIR=$(pwd)
 
+	# Code has successfully passed valgrind with no memory leaks so far
     #valgrind --leak-check=full /home/brandon/Documents/meta/img vid_frames/out_0001.png processed_vid_frames/2.png
     
-    if [ -e processed_vid_frames/2.png ]
+	# Show the frame we're working with
+    if [ -e $IMAGE ]
     then
-        eog processed_vid_frames/2.png &
+        eog $IMAGE &
     fi
+	
+	# Run on the example frame
+	./$BASE $IMAGE "out_$IMAGE"
 
-    ./plot.py $DIR/processed_vid_frames/$fName
+	# Run plot.py on the frame (wherein matplotlib automatically scales the )
+    ./plot.py "out_$IMAGE"
+	
+	eog "fig_out_$IMAGE"
+	
+	# So there are two images created here:
+	# "out_$IMAGE" is where the processed data are written
+	# "fig_out_$IMAGE" is created by plot.py
 
+	# For looping these steps, etc.
     #avconv -r 30 -i "to_combine_2/out_out_%04d.png" -y $OUT
 	
 	# Send me an email to notify of the processes' completion
